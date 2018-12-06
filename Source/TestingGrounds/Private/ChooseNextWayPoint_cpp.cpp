a9ccb9b2563493464f987d63a4c9a6ff0008f700
WIN-20160721KQO\Administrator <manazusorata@gmail.com>
2018-12-06 17:03:51 +0800

    TS04:Finish the  waypoint_behavior with BP,start code in VS

---
+++ Source/TestingGrounds/Private/ChooseNextWayPoint_cpp.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChooseNextWayPoint_cpp.h"
+
+
+
+
+EBTNodeResult::Type UChooseNextWayPoint_cpp::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp,Warning,TEXT("AI in c++ YEAH~!"))
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/TestingGrounds/Public/ChooseNextWayPoint_cpp.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWayPoint_cpp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWayPoint_cpp : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+};
